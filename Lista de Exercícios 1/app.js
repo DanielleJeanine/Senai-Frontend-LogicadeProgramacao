8f7b43ec3a9ad2b9e378cce2e84ef974fbf075db
Danielle Jeanine da Silva <PET0144658@aluno.firjansenaisesi.com.br>
2023-09-11 21:31:24 -0300

    Começo da Lista de Exercícios 1

---
+++ Lista de Exercícios 1/app.js
@@ -0,0 +1,20 @@
+function exercicio1(){
+
+   let num1 = parseInt(prompt("Informe o primeiro número:"));
+   let  num2 = parseInt(prompt("Informe o segundo número:"));
+   let resultado = num1 + num2;
+
+   alert(`A soma de ${num1} e  ${num2} é igual a ${resultado}`);
+
+
+   function exercicio2(){
+    let nome = prompt("Informe o nome: ");
+    let sobrenome = prompt("Informe o sobrenome: ");
+
+    alert (`Olá, ${nome} ${sobrenome}`)
+   }
+
+   exercicio2();
+
+
+}
