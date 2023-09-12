21ba59f6c7b817a9a8c23744cec0ccc847552bcd
DanielleJeanine <danielle.jeanine92@gmail.com>
2023-09-12 17:13:44 -0300

    Exercicios 3, 4 e 5

--- Lista de Exercícios 1/app.js
+++ Lista de Exercícios 1/app.js
@@ -6,15 +6,39 @@ function exercicio1(){
 
    alert(`A soma de ${num1} e  ${num2} é igual a ${resultado}`);
 
+ }
+
 
    function exercicio2(){
     let nome = prompt("Informe o nome: ");
     let sobrenome = prompt("Informe o sobrenome: ");
 
-    alert (`Olá, ${nome} ${sobrenome}`)
+    alert (`Olá, ${nome} ${sobrenome}`);
    }
 
-   exercicio2();
+  
+   function exercicio3(){
+      
+      let valorEmReal = parseFloat(prompt("Informe o valor a ser convertido:"));
+      const cotacaoDolar = 4.95;
+      let valorConvertido = valorEmReal/cotacaoDolar;
+      valorConvertido = valorConvertido.toFixed(2);
+
+      alert(`O valor de R$${valorEmReal} é equivalente  a $${valorConvertido}`)
+   }
+
+   function exercicio4(){
+      let num = parseInt(prompt("Informe um número:"));
+      let antecessor = num - 1;
+      let sucessor = num + 1;
+
+      alert(`O antecessor de ${num} é ${antecessor} e o sucessor é ${sucessor}`);
 
 
-}
+   }
+
+   function exercicio5(){
+      
+   }
+
+   exercicio4();
